596bd867436f2a500b78cc17ace9d00c013c7e27
Roman <roman.baytala@gmail.com>
2026-02-26 10:55:34 +0200

    Add config files. Add user model.

---
+++ app/models/user.py
@@ -0,0 +1,30 @@
+from ..config.ext import db
+
+class User(db.Model):
+    __timetable__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(240), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.name} {self.surname}>'
+    
+    def put_into_dto(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'surname': self.surname,
+            'email': self.email
+        }
+    
+    @staticmethod
+    def get_from_dto(dto):
+        return User(
+            name=dto['name'],
+            surname=dto['surname'],
+            email=dto['email'],
+            password=dto['password']
+        )
